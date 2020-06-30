7a34fdfd9141af05a3887f984d02644eb7f77d57
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-01 08:44:12 +0900

    designed sign_in page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_005548) do
+ActiveRecord::Schema.define(version: 2020_06_30_231629) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 2020_06_30_005548) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", limit: 100, default: "", null: false
+    t.string "self_introduction", limit: 500
+    t.string "img_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
