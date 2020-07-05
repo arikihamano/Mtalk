7c0c7757a052c7a55c0ce2b03a1577bebd1c9e55
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-05 10:24:40 +0900

    video1:18:00 implemented to connect different room

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,33 +1,36 @@
 import consumer from "./consumer";
 
-consumer.subscriptions.create(
-  { channel: "RoomChannel", room_id: 3 },
-  {
-    connected() {
-      console.log("connected...");
-      // Called when the subscription is ready for use on the server
-    },
+document.addEventListener("turbolinks:load", () => {
+  const room_element = document.getElementById("room-id");
+  const room_id = Number(room_element.getAttribute("data-room-id"));
 
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
+  consumer.subscriptions.create(
+    { channel: "RoomChannel", room_id: room_id },
+    {
+      connected() {
+        console.log("connected to " + room_id);
+        // Called when the subscription is ready for use on the server
+      },
 
-    received(data) {
-      console.log(data);
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
 
-      const element = document.getElementById("user-id");
-      const user_id = Number(element.getAttribute("data-user-id"));
+      received(data) {
+        const user_element = document.getElementById("user-id");
+        const user_id = Number(user_element.getAttribute("data-user-id"));
 
-      let html;
+        let html;
 
-      if (user_id === data.message.user_id) {
-        html = data.mine;
-      } else {
-        html = data.theirs;
-      }
+        if (user_id === data.message.user_id) {
+          html = data.mine;
+        } else {
+          html = data.theirs;
+        }
 
-      const messageContainer = document.getElementById("messages");
-      messageContainer.innerHTML = messageContainer.innerHTML + html;
-    },
-  }
-);
+        const messageContainer = document.getElementById("messages");
+        messageContainer.innerHTML = messageContainer.innerHTML + html;
+      },
+    }
+  );
+});
