96d8004559ab1cc2b22304dc33611638370e3785
arikihamano <arikid_boxing_0408@yahoo.co.jp>
2020-07-07 09:07:01 +0900

    cf.3-3 implemented users/show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :messages
   resources :rooms
-  resources :users, only: %i(show)
+  resources :users, only: %i(index show)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "rooms#index"
+  root "top#index"
 end
